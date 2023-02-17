314ff12b03cc02a2ee98da7ba0b500cbfd6e01c4
KANAYAMA <japan.tokai.k.r@gmail.com>
2023-02-17 22:10:28 +0900

    Upload c# project

---
+++ CodeGenius/CodeGenius/TextGene.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using MeCab;
+
+static class UseMecab
+{
+    public static void excute()
+    {
+        var sentence = "マリーアントワネット演じてるだけ";
+        var parameter = new MeCabParam();
+        var tagger = MeCabTagger.Create(parameter);
+        
+        foreach (var node in tagger.ParseToNodes(sentence))
+        {
+            if (node.CharType > 0)
+            {
+                var features = node.Feature.Split(',');
+                var displayFeatures = string.Join(", ", features);
+
+                Console.WriteLine($"{node.Surface}\t{displayFeatures}");
+            }
+        }
+    }
+}
+
+/// <summary>
+/// Test用Class
+/// </summary>
+static class Test
+{
+    /// <summary>
+    /// Entry Point
+    /// </summary>
+    static void Main()
+    {
+        UseMecab.excute();
+    }
+}
+
+
